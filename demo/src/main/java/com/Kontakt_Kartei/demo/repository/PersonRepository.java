0d9762fa606873787ca3ccf525c22d54d10c328d
TheAmarok <148391429+TheAmarok@users.noreply.github.com>
2026-07-13 14:29:37 +0200

    Revert "fehlgeschlagener Versuch"
    This reverts commit 57a92299ddbc63ec23c86bda2517b4ae0e308f1f.

--- demo/src/main/java/com/Kontakt_Kartei/demo/repository/PersonRepository.java
+++ demo/src/main/java/com/Kontakt_Kartei/demo/repository/PersonRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Kontakt_Kartei.demo.entity.PersonEntity;
 
-import java.util.List;
-
 public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
-    List<PersonEntity> findByBereiche_Bereich_id(Long bereich_id);
+
 }
