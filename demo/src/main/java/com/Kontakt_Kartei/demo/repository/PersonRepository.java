57a92299ddbc63ec23c86bda2517b4ae0e308f1f
TheAmarok <148391429+TheAmarok@users.noreply.github.com>
2026-07-13 14:15:36 +0200

    fehlgeschlagener Versuch

--- demo/src/main/java/com/Kontakt_Kartei/demo/repository/PersonRepository.java
+++ demo/src/main/java/com/Kontakt_Kartei/demo/repository/PersonRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Kontakt_Kartei.demo.entity.PersonEntity;
 
-public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
+import java.util.List;
 
+public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
+    List<PersonEntity> findByBereiche_Bereich_id(Long bereich_id);
 }
